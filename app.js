7dcdc29dda58d216d07a71b7fe20a68027d21094
Rasujon3 <rasujon3@gmail.com>
2022-04-05 13:36:04 +0600

    undefined route handle

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = require("./src/routes/api")
 const app = new express();
+
 app.use("/api/v1",router);
+
+// Undefined Route
+app.use('*',(req,res)=> {
+    res.status(404).json({status: "fail", data: "Not Found"})
+})
 
 module.exports=app;
